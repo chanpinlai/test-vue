d6c4d3db523879425c51d8f64e1d377569666d37
chanpinlai <jakgcc@gmail.com>
2023-01-29 17:55:35 +0800

    add nav header.js

---
+++ public/header.js
@@ -0,0 +1,21 @@
+var header = '<ol>' +
+'<li><a href="index.html">index</a></li>' +
+'<li><a href="demo.html">demo</a></li>' +
+'<li><a href="convertMoney.html">convertMoney</a></li>' +
+'<li><a href="directive.html">directive指令</a></li>' +
+'<li><a href="event.html">event事件處理</a></li>' +
+'<li><a href="ifShowFor.html">v-if,v-show,v-for</a></li>' +
+'<li><a href="lifeCycle.html">lifeCycle</a></li>' +
+'<li><a href="addToMessage.html">新增訊息</a></li>' +
+'<li><a href="component.html">元件特性</a></li>' +
+'<li><a href="parentChild.html">父子元件</a></li>' +
+'<li><a href="parentChild2.html">父子元件2</a></li>' +
+'<li><a href="propsEmit.html">v-model 與元件的雙向綁定 (Vue 3.x 新增)</a></li>' +
+'<li><a href="provideInject.html">跨越層級的傳遞方式provide inject</a></li>' +
+'<li><a href="VueCompositionAPI.html">Vue Composition API</a></li>' +
+'</ol>'+
+'<style lang="scss">a {' +
+'color: #3eaf7c;' +
+'text-decoration: none;' +
+'  }</style>';
+document.write(header);
