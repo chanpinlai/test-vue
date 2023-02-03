7c5d890c50a8f9545f1a75f7039b662c531361d7
chanpinlai <jakgcc@gmail.com>
2023-02-03 12:07:01 +0800

    add 動態元件

--- public/header.js
+++ public/header.js
@@ -13,6 +13,7 @@ var header = '<ol>' +
 '<li><a href="propsEmit.html">v-model 與元件的雙向綁定 (Vue 3.x 新增)</a></li>' +
 '<li><a href="provideInject.html">跨越層級的傳遞方式provide inject</a></li>' +
 '<li><a href="VueCompositionAPI.html">Vue Composition API</a></li>' +
+'<li><a href="dymainComponent.html">動態元件管理</a></li>' +
 '</ol>'+
 '<style lang="scss">a {' +
 'color: #3eaf7c;' +
